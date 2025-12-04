bbad7d1fe17fe23ef554954196f68bd8b0c83199
riririmmm <mr8774@smail.kongju.ac.kr>
2025-12-04 18:46:55 +0900

    줄 정렬

--- src/main/java/com/example/demo/project/api/dto/NewPageComment.java
+++ src/main/java/com/example/demo/project/api/dto/NewPageComment.java
@@ -4,12 +4,27 @@ public class NewPageComment {
     private Integer page;
     private String comment;
 
-    public NewPageComment() {}
-    public NewPageComment(Integer page, String comment) {
-        this.page = page; this.comment = comment;
+    public NewPageComment() {
     }
-    public Integer getPage() { return page; }
-    public String getComment() { return comment; }
-    public void setPage(Integer page) { this.page = page; }
-    public void setComment(String comment) { this.comment = comment; }
+
+    public NewPageComment(Integer page, String comment) {
+        this.page = page;
+        this.comment = comment;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
--- src/main/java/com/example/demo/project/api/dto/ProgressReq.java
+++ src/main/java/com/example/demo/project/api/dto/ProgressReq.java
@@ -2,8 +2,19 @@ package com.example.demo.project.api.dto;
 
 public class ProgressReq {
     private Integer currentPage;
-    public ProgressReq() {}
-    public ProgressReq(Integer currentPage) { this.currentPage = currentPage; }
-    public Integer getCurrentPage() { return currentPage; }
-    public void setCurrentPage(Integer currentPage) { this.currentPage = currentPage; }
+
+    public ProgressReq() {
+    }
+
+    public ProgressReq(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public Integer getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
 }
